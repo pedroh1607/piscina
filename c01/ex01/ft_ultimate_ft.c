cb87e428db17f17e1c2ed57f9b71f29e27a547fe
pedro henrique <107113002+pedroh1607@users.noreply.github.com>
2025-08-06 12:42:26 -0300

    Update ft_ultimate_ft.c

--- c01/ex01/ft_ultimate_ft.c
+++ c01/ex01/ft_ultimate_ft.c
@@ -1,41 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cleiton.c                                          :+:      :+:    :+:   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pandrade <pandrade@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 09:12:32 by pandrade          #+#    #+#             */
-/*   Updated: 2025/08/04 14:36:13 by pandrade         ###   ########.fr       */
+/*   Updated: 2025/08/06 12:29:46 by pandrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 #include <stdio.h>
-void ft_ultimate_ft(int *********nbr)
+
+void	ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
 
-int main(void)
+int	main(void)
 {	
-	int x = 0;
+	int	x;
 
-	int *nbr1 = &x;
-	int **nbr2 = &nbr1;
-	int ***nbr3 = &nbr2;
-	int ****nbr4 = &nbr3;
-	int *****nbr5 = &nbr4;
-	int ******nbr6 = &nbr5;
-	int *******nbr7 = &nbr6;
-	int ********nbr8 = &nbr7;
+	int	*nbr1;
+
+	int	**nbr2;
+
+	int	***nbr3;
+
+	int	****nbr4;
+
+	int	*****nbr5;
+
+	int	******nbr6;
+
+	int	*******nbr7;
+
+	int	********nbr8;
 	
-	
+	x = 0;
+	nbr1 = &x;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+
 	ft_ultimate_ft(&nbr8);
 
 	printf("%d", x);
 
 	return 0;
 }
-
 
 
